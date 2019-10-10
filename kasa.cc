63e4383a29ca489b99eef3ca5da9f0e40f45b102
Konrad <konrad@Konrads-MacBook-Pro.local>
2019-10-10 10:11:01 +0200

    dogadanie sie co do wygladu funkcji

---
+++ kasa.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// nazwa biletu, cena, długość działąnia
+using ticket = pair<string, pair<int, int>>;
+// id przystanku, czas zatrzymania
+using stop = pair<int, int>;
+
+// stops - the first parameter is the id of the stop, the second
+// one is the time of the stop, whis is number of minuts from 00:00
+void add_route(int route_id, vector<stop> stops) {
+
+}
+
+void add_ticket(ticket t) {
+
+}
+
+// para - <przystanek, numer kursu>
+// ostatnia para ma numer kursu = 0
+string query(vector <pair<int, int> > q) {
+
+}
+
+int main() {
+
+}
